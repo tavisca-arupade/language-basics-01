7edac079f080504a48066aac31de208a27894909
tavisca-arupade <51902733+tavisca-arupade@users.noreply.github.com>
2019-06-25 19:58:56 +0530

    Added suggested changes

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -1,17 +1,51 @@
-﻿using System;
+using System;
+using System.Text.RegularExpressions;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
     class Program
     {
+        static string missing = "?";
+        static int impossible = -1;
+
         static void Main(string[] args)
         {
+
             Test("42*47=1?74", 9);
             Test("4?*47=1974", 2);
             Test("42*?7=1974", 4);
             Test("42*?47=1974", -1);
             Test("2*12?=247", -1);
+            
+            try
+            {
+                Test("23*1k3=628", 4);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.GetType().FullName);
+                Console.WriteLine(fe.Message);
+            }
+            try
+            { 
+                Test("", 0);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.GetType().FullName);
+                Console.WriteLine(ae.Message);
+            }
+            try
+            {
+                Test("23*89*45=78?9", 1);
+            }
+            catch(ArgumentException ae)
+            {
+                Console.WriteLine(ae.GetType().FullName);
+                Console.WriteLine(ae.Message);
+            }
             Console.ReadKey(true);
+
         }
 
         private static void Test(string args, int expected)
@@ -24,49 +58,38 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         {
             // Author : Aditi Rupade.
 
+            //check for null string
+
+            if (String.IsNullOrEmpty(equation))
+                throw new ArgumentException("argument cannot be null or empty", "equation");
+
             //Splitting string a*b=c into a,b,c
             string[] splitEquation = equation.Split(new Char[] { '*', '=' });
 
+            //check for equation form , i.e. a*b=c
+            if (splitEquation.Length != 3)
+                throw new ArgumentException("argument must be of the form a * b = c", "equation");
+
+            string number1 = splitEquation[0];
+            string number2 = splitEquation[1];
+            string result = splitEquation[2];
+
+            // check for only one ? mark and no other characters
+            if (!Regex.IsMatch(splitEquation[0], @"^[0-9?0-9]+$" ) || !Regex.IsMatch(splitEquation[1], @"^[0-9?0-9]+$") || !Regex.IsMatch(splitEquation[2], @"^[0-9?0-9]+$"))
+                throw new FormatException("enter string in correct format(just one ? mark and all other digits)");
+
             //Case1: num1 has ?
 
-            if (splitEquation[0].Contains('?'))
+            if (number1.Contains(missing))
             {
-                 int num2 = 0, res = 0;
-
-                //Converting String to Integer
-                Int32.TryParse(splitEquation[1], out num2);
-                Int32.TryParse(splitEquation[2], out res);
-
-               //Checking for factors of result(c)
-                if (res % num2 == 0)
-                {
-                    //calculating factor with missing number
-                    int temp = res / num2;
-                    string temp2 = temp.ToString();
-
-                    return FindIndex(splitEquation[0], temp2);
-                }
-
+                return FindNumberBeforeAssignment(number1, number2, result);
             }
 
             //Case2: num2 has ?
 
-            else if (splitEquation[1].Contains('?'))
+            else if (number2.Contains(missing))
             {
-                //Works in the Same way as above function just values different
-                int num1 = 0, res = 0;
-
-                Int32.TryParse(splitEquation[0], out num1);
-                Int32.TryParse(splitEquation[2], out res);
-
-                if (res % num1 == 0)
-                {
-                    int temp = res / num1;
-                    string temp2 = temp.ToString();
-
-                    return FindIndex(splitEquation[1], temp2);
-                }
-
+                return FindNumberBeforeAssignment(number2, number1, result);
             }
 
             //Case3: res has ?
@@ -75,32 +98,51 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             {
                 int num1 = 0, num2 = 0;
 
-                Int32.TryParse(splitEquation[0], out num1);
-                Int32.TryParse(splitEquation[1], out num2);
+                Int32.TryParse(number1, out num1);
+                Int32.TryParse(number2, out num2);
 
                 //calculating actual result
                 int temp = num1 * num2;
                 string temp2 = temp.ToString();
 
-                return FindIndex(splitEquation[2], temp2);
+                return GetMissingNumber(result, temp2);
             }
-            Console.WriteLine($"-1");
-            return -1;
         }
-        
-         public static int FindIndex(string equationVar, string tempVar)
+
+        public static int FindNumberBeforeAssignment(string number1, string number2, string result)
+        {
+            int num2 = 0, res = 0;
+
+            //Converting String to Integer
+
+            Int32.TryParse(number2, out num2);
+            Int32.TryParse(result, out res);
+
+            double tempNumber = (double) res / num2;
+
+            //Checking for decimal in result(c)
+            if (tempNumber - Convert.ToInt32(tempNumber) == 0)
+            {
+
+                string temp = tempNumber.ToString();
+
+                return GetMissingNumber(number1, temp);
+            }
+            return impossible;
+        }
+
+        public static int GetMissingNumber(string equationValue, string calculatedValue)
         {
             //checking whether equation number and temporary number are of same magnitude
-            if (equationVar.Length == tempVar.Length)
+            if (equationValue.Length == calculatedValue.Length)
             {
                 //Finding ? to replace missing number 
-                int index = equationVar.IndexOf('?');
-                Console.WriteLine(tempVar[index]);
+                int index = equationValue.IndexOf('?');
 
-                return tempVar[index] - '0';
+                // this is to avoid returning ASCII value of the string instead it returns the number in int type
+                return calculatedValue[index] - '0';
             }
-            Console.WriteLine($"-1");
-            return -1;
+            return impossible;
         }
     }
 }
